927da4637253e0a8195fb4622f13b204cb9062b5
Sandeep Tripathi <119596263+Sandyvaranasi@users.noreply.github.com>
2023-07-06 23:30:14 +0530

    added functions and pass statement

---
+++ functions.py
@@ -0,0 +1,12 @@
+# Functional statements and expressions in python
+# two types 1) Build in function 2) User defined function
+
+def avg(a,b):
+    mean = (a+b)/2
+    print(mean)
+
+avg(2,3) #calling a function
+
+# Pass
+def skipFnBody(a,b):
+    pass # To leave function without body in order to write it later till then pass will ignore error
